7795a283695d6ff93fcd554b91dfe0ef3be09d5a
Chien-Chi Lo <clo@cori11.nersc.gov>
2019-11-18 10:17:02 -0800

    adjust OPENMP thread

--- MAGsPipeline/submit_SRR7877884.sh
+++ MAGsPipeline/submit_SRR7877884.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #SBATCH --qos=regular
-#SBATCH --time=1800:00
+#SBATCH --time=48:00:00
 #SBATCH --output=/global/project/projectdirs/m3408/aim2/metagenome/MAGs/SRR7877884.log
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task 24
+#SBATCH --cpus-per-task 32
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=lo.chienchi@gmail.com
 #SBATCH --constraint=knl
 #SBATCH --account=m3408
 #SBATCH --job-name=MAGs_SRR7877884
+
+#OpenMP settings:
+export OMP_NUM_THREADS=8
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
 
 cd /global/project/projectdirs/m3408/aim2/metagenome/MAGs
 
