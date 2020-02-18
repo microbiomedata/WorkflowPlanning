c5ecb9d6f83fbbe7d0f79de91968209a7ab0e8d0
Chien-Chi Lo <clo@cori09.nersc.gov>
2020-02-18 11:26:30 -0800

    rename SlurmCromwellJtmShifter to CromwellJtmShifter

---
+++ MAGsPipeline/CromwellJtmShifter/sbatch_jtm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --qos=regular
+#SBATCH --time=1800:00
+#SBATCH --output=/global/cfs/projectdirs/m3408/aim2/metagenome/MAGs/jtm_test.log
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task 32
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=lo.chienchi@gmail.com
+#SBATCH --constraint=haswell
+#SBATCH --account=m3408
+#SBATCH --job-name=MAGs_SRR2126941
+
+#OpenMP settings:
+#export OMP_NUM_THREADS=8
+#export OMP_PLACES=threads
+#export OMP_PROC_BIND=spread
+
+cd /global/cfs/projectdirs/m3408/aim2/metagenome/MAGs
+
+export JTM_HOST_NAME=cori
+# starting JTM
+source /global/cfs/projectdirs/m3408/aim2/metagenome/venv/bin/activate_jtm.sh
+jgi-task-manager &
+sleep 10
+jtm-worker -tp aim2_test_pool &
+
+java -XX:ParallelGCThreads=32 -Dconfig.file=jtm.conf -jar /global/common/software/m3408/cromwell-45.jar run -i mag_input_w_pname.json MAGgeneration_jtmShifter.wdl
